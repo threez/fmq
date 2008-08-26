a8b668bd3f0ea4a2482d934bf38b7313e63218e2
Vincent Landgraf <fmq-3z@gmx.net>
2008-08-26 20:47:39 +0200

    added a rack implementation and removed the mongrel specific server, also added a config stub (config.ru)

---
+++ default-server/config.ru
@@ -0,0 +1,86 @@
+require "fmq"
+
+# load all local queues (from project directory)
+Dir.glob("queues/*.rb").each { |f| require f }
+
+# setup log level of free message queue
+FreeMessageQueue.log_level("info")
+
+# =====================================================
+#        create and configure the queue manager
+# =====================================================
+queue_manager = FreeMessageQueue::QueueManager.new()
+
+queue_manager.setup do |qm|
+  # if someone pushes to a queue that don't exists
+  # the queue manager will create one for you if the option
+  # <em>auto_create_queues</em> is <b>true</b>. This
+  # is a useful option if you are in development,
+  # the queue will be a FreeMessageQueue::SyncronizedQueue by default
+  qm.auto_create_queues = true
+  
+  # =====================================================
+  #      if you want some queues right from startup
+  #        define there url and constraints here
+  # =====================================================
+  
+  qm.setup_queue do |q|
+    # the path to the queue e.g. /app1/myframe/test1
+    # means http://localhost:5884/app1/myframe/test1
+    # this parameter is not optional
+    q.path = "/fmq_test/test1"
+    # this defines the maximum count of messages that 
+    # can be in the queue, if the queue is full every
+    # new message will be rejected with a http error
+    # this parameter is optional if you don't specify
+    # a max value the queue size depends on your system
+    q.max_messages = 1000000
+    # this optional to and specifys the max content size
+    # for all data of a queue
+    # valid extensions are kb, mb, gb
+    q.max_size = "10kb"
+  end
+  
+  # if you want you can specify the class of the queue
+  # this is interessting if you write your own queues
+  qm.setup_queue FreeMessageQueue::LoadBalancedQueue do |q|
+    q.path = "/fmq_test/test2"
+  end
+  
+  # if you have special queues include put them into the queues
+  # folder and and use them (this MyTestQueue is places in queues/mytest.rb)
+  qm.setup_queue MyTestQueue do |q|
+    q.path = "/fmq_test/test3"
+  end
+  
+  # this is a forwarding queue wich forwards one message
+  # to some other queues
+  qm.setup_queue FreeMessageQueue::ForwardQueue do |q|
+    q.path = "/fmq_test/forward_to_1_and_2"
+    # you can add as may queues as you want
+    # but seperate them with a space char
+    q.forward_to = ["/fmq_test/test1", "/fmq_test/test2"]
+  end
+end
+
+# =====================================================
+#           setup the ajax admin interface
+# =====================================================
+
+# handle requests to remove, create, update queues
+map "/admin/backend" do
+  run FreeMessageQueue::AdminInterface.new(queue_manager)
+end
+
+# serve static files in admin-interface folder
+map "/admin" do
+  run Rack::File.new("./admin-interface")
+end
+
+# =====================================================
+#      install the server for the queue maneger
+# =====================================================
+
+map "/" do
+  run FreeMessageQueue::Server.new(queue_manager)
+end
---
+++ lib/fmq/server.rb
@@ -0,0 +1,144 @@
+#!/usr/bin/env ruby -wKU
+#
+# Copyright (c) 2008 Vincent Landgraf
+#
+# This file is part of the Free Message Queue.
+# 
+# Free Message Queue is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Free Message Queue is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Free Message Queue.  If not, see <http://www.gnu.org/licenses/>.
+#
+begin
+  require "rack"
+rescue LoadError
+  require "rubygems"
+  require "rack"
+end
+
+module FreeMessageQueue
+  # This implements server that plugs the free message queue into a rack enviroment
+  class Server
+    # When creating a server you have to pass the QueueManager
+    def initialize(queue_manager)
+      @queue_manager = queue_manager
+      @log = FreeMessageQueue.logger
+    end
+  
+    # Process incoming request and send them to the right sub processing method like <em>process_get</em>
+    def call(env)
+      request = Rack::Request.new(env)
+      queue_path = request.env["REQUEST_PATH"]
+      @log.debug("[Server] Request params: #{YAML.dump(request.params)})")
+      
+      response = nil
+      begin
+        # process supported features
+        if request.request_method.match(/^(GET|POST|HEAD|DELETE)$/) then
+          response = self.send("process_" + request.request_method.downcase, request, queue_path)
+        else
+          response = client_exception(request, queue_path, 
+            ArgumentError.new("[Server] Method is not supported '#{method}'"))
+        end
+      rescue QueueManagerException => ex
+        response = client_exception(request, queue_path, ex)
+      end
+      
+      response.header["SERVER"] = SERVER_HEADER
+      return response.finish
+    end
+  
+  protected
+  
+    # Returns an item from queue and sends it to the client.
+    # If there is no item to fetch send an 204 (NoContent) and same as HEAD
+    def process_get(request, queue_path)
+      message = @queue_manager.poll(queue_path)
+            
+      unless message.nil? then
+        response = Rack::Response.new([], 200)
+        
+        @log.debug("[Server] Response to GET (200)")
+        response.header["CONTENT-TYPE"] = message.content_type
+        response.header["QUEUE_SIZE"] = @queue_manager.queue_size(queue_path).to_s
+        response.header["QUEUE_BYTES"] = @queue_manager.queue_bytes(queue_path).to_s
+        
+        # send all options of the message back to the client
+        if message.respond_to?(:option) && message.option.size > 0
+          for option_name in message.option.keys
+            response.header["MESSAGE_#{option_name.gsub("-", "_").upcase}"] = message.option[option_name].to_s
+          end
+        end
+        
+        if !message.payload.nil? && message.bytes > 0
+          @log.debug("[Server] Message payload: #{message.payload}")
+          response.write(message.payload)
+        end
+      else
+        response = Rack::Response.new([], 204)
+        @log.debug("[Server] Response to GET (204)")
+        response.header["QUEUE_SIZE"] = response["QUEUE_BYTES"] = 0.to_s
+      end
+      return response
+    end
+    
+    # Put new item to the queue and and return sam e as head action (HTTP 200)
+    def process_post(request, queue_path)
+      @log.debug("[Server] Response to POST (200)")
+      post_body = request.body.read
+      message = Message.new(post_body, request.content_type)
+      @log.debug("[Server] Message payload: #{post_body}")
+       
+      # send all options of the message back to the client
+      for option_name in request.env.keys
+        if option_name.match(/HTTP_MESSAGE_([a-zA-Z][a-zA-Z0-9_\-]*)/)
+          message.option[$1] = request.env[option_name]
+        end
+      end
+      
+      @queue_manager.put(queue_path, message)
+      
+      response = Rack::Response.new([], 200)
+      response.header["QUEUE_SIZE"] = @queue_manager.queue_size(queue_path).to_s
+      response.header["QUEUE_BYTES"] = @queue_manager.queue_bytes(queue_path).to_s
+      return response
+    end
+    
+    # Just return server header and queue size (HTTP 200)
+    def process_head(request, queue_path)
+      @log.debug("[Server] Response to HEAD (200)")
+      
+      response = Rack::Response.new([], 200)
+      response.header["QUEUE_SIZE"] = @queue_manager.queue_size(queue_path).to_s
+      response.header["QUEUE_BYTES"] = @queue_manager.queue_bytes(queue_path).to_s
+      return response
+    end
+    
+    # Delete the queue and return server header (HTTP 200)
+    def process_delete(request, queue_path)
+      @log.debug("[Server] Response to DELETE (200)")
+      @queue_manager.delete_queue(queue_path)
+
+      response = Rack::Response.new([], 200)
+    end
+    
+    # Inform the client that he did something wrong (HTTP 400).
+    # HTTP-Header field Error contains information about the problem.
+    # The client errorwill also be reported to warn level of logger.
+    def client_exception(request, queue_path, ex)
+      @log.warn("[Server] Client error: #{ex}")
+      
+      response = Rack::Response.new([], 400)
+      response.header["ERROR"] = ex.message
+      return response
+    end
+  end
+end
